8af3e2f023a668aba2a6641269ccde8a55592c12
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2021-11-09 20:45:50 -0500

    starting components

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Footer = () => {
+	return (
+	    <div>
+	      <h1>Footer</h1>
+	    </div>
+	  )
+}
+
+export default Footer
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Header = () => {
+	return (
+	    <div>
+	      <h1>HEADER</h1>
+	    </div>
+	  )
+}
+
+export default Header
