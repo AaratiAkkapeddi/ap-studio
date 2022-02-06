e6208faae4f251949bc3ef7d73b4118b2ba308c0
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2022-02-06 18:58:39 -0500

    fixes hp text column issues

--- src/components/thumb.js
+++ src/components/thumb.js
@@ -5,10 +5,13 @@ import { StaticImage } from "gatsby-plugin-image"
 const Thumb = ({imageurl, videourl, name}) => {
 
 	if(videourl && videourl.length > 0){
+		let imgsizes = imageurl + "/-/resize/x480/ 480w, " + imageurl + "/-/resize/x500/ 800w"
 		return (
 			    <div className="thumb-item">
 				    <video muted loop autoPlay>
 				      <source src={videourl} type="video/mp4"/>
+				      <img src={imageurl+"/-/resize/x480/"} srcSet={imgsizes} sizes="(max-width: 600px) 480px,
+            800px" alt={name}/>
 				    </video>
 			    </div>
 			  )
